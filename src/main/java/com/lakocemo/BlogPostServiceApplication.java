660c38e009c2c628b03765afecf8a0569e3ac882
danilotosic <danilo.ns@live.com>
2017-05-10 13:05:07 +0200

    initial commit

---
+++ src/main/java/com/lakocemo/BlogPostServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lakocemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPostServiceApplication.class, args);
+	}
+}
